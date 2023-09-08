027c670ec98eff159e467f7b5bfe199fa215c959
infotech87871 <infotech87871@gmail.com>
2023-09-08 22:10:44 +0530

    1st Push for Costumes

---
+++ src/test/java/org/gitpushpull/GitPushPull.java
@@ -0,0 +1,23 @@
+package org.gitpushpull;
+
+public class GitPushPull {
+
+	private void method() {
+	
+	byte companyAddress = 50;
+	System.out.println("Company Age : "+companyAddress);
+	
+	short companyId = 1616;
+	System.out.println("Company Id : "+companyId);
+	
+	int companyCode = 4387545;
+	System.out.println("Company Code : "+companyCode);
+	
+	long companyNumber = 7538874657l;
+	System.out.println("Company Number : "+companyNumber);
+	
+	float minimumSalary = 80000.3434454f;
+	System.out.println("Minimum Salary : "+minimumSalary);
+
+}
+}
